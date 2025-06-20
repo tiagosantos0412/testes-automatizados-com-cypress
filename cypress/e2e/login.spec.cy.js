7be14da6537ce9992f6f76852e74679f283fe225
Tiago <tiagosantos0412@gmail.com>
2025-06-20 10:36:53 -0300

    Criando um objeto userDate com os dados de login e senha para os testes.

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -8,18 +8,29 @@ describe('Orange HRM Tests', () => {
     wrongAlert: '.oxd-alert--error'
   }
 
+  const userData = {
+    userSuccess: {
+      userName: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      userName: 'teste',
+      password: 'teste'
+    }
+  }
+
   it('Login success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.userNameField).type('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.userNameField).type(userData.userSuccess.userName)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorList.dashboardGrid)
   })
   it('Login Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.userNameField).type('teste')
-    cy.get(selectorList.passwordField).type('teste')
+    cy.get(selectorList.userNameField).type(userData.userFail.userName)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongAlert)
   })
